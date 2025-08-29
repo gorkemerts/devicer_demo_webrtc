24694d6f8a7bb84116346808e56a7dd7de8e1026
gorkemerts <gorkem.mert@virgosol.com>
2025-08-29 15:16:59 +0300

    feat : flow completed:

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ port : "3000"});
 let streamer_ws = null ; 
+let viever_ws = null ; 
 // public klasörünü servis et
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,7 +16,7 @@ wss.on('connection', ws => {
 
     ws.on('message' , data => {
         const message = JSON.parse(data.toString());
-
+        console.log("ws_message: ", message);
         switch (message.type) {
             case "streamer_awake" : 
               console.log(message)
@@ -24,67 +25,25 @@ wss.on('connection', ws => {
                 break ; 
             case 'client_offers' :
                 console.log(message) ; 
-                console.log("mesag streamere iletiliyor") ; 
-                streamer_ws.send(JSON.stringify({type : "client_offers"}));
+                console.log("mesaj streamere iletiliyor") ; 
+                streamer_ws.send(JSON.stringify({type : "clientoffers"}));
                 break ; 
+            case "streamer_candidate" : 
+                console.log("streamer_candidate_has_come");
+                viever_ws.send(JSON.stringify(message));
             case 'client_awake' : 
-                console.log(message);
+                viever_ws = ws ; 
                 break;
-
+            case 'stream_offer' : 
+                console.log("streamoffersended");
+                viever_ws.send(JSON.stringify(message)); 
+                break ; 
+            case 'answer' : 
+                streamer_ws.send(JSON.stringify(message)) ; 
+                break ; 
             }          
              
-})
-
-
-    
-  const pc = new wrtc.RTCPeerConnection({
-    iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
-  });
-
-  // DataChannel oluştur
-  const dc = pc.createDataChannel('server-to-browser');
-  
-  dc.onopen = () => {
-    console.log("DataChannel açık!");
-
-    // Her saniye mesaj gönder
-    let count = 0;
-    const interval = setInterval(() => {
-      if (dc.readyState === 'open') {
-        dc.send(`Merhaba Browser! Mesaj #${++count} Node.js'ten geliyorum.`);
-      } else {
-        clearInterval(interval); 
-      }
-    }, );
-  };
-
-  dc.onmessage = msg => console.log("Browser'dan gelen mesaj:", msg.data);
-
-  // ICE candidate geldiğinde WebSocket ile gönder
-  pc.onicecandidate = ({ candidate }) => {
-    if (candidate) ws.send(JSON.stringify({ type: 'candidate', candidate }));
-  };
-
-  // Browser'dan gelen mesajları handle et
-  ws.on('message', async message => {
-    const data = JSON.parse(message);
-
-    if (data.type === 'answer') {
-      await pc.setRemoteDescription(data.answer);
-    } else if (data.type === 'candidate') {
-        console.log("mine",data.candidate);
-      await pc.addIceCandidate(data.candidate);
-      console.log("ICE candidate eklendi.");
-    }
-  });
-
-  // Offer oluştur ve WebSocket ile gönder
-  (async () => {
-    const offer = await pc.createOffer();
-    await pc.setLocalDescription(offer);
-    ws.send(JSON.stringify({ type: 'offer', offer }));
-    console.log("Offer gönderildi.");
-  })();
+}) 
 });
 
 server.listen(8080, () => {
--- streamer_service.js
+++ streamer_service.js
@@ -3,24 +3,43 @@ const ws = new WebSocket('ws://localhost:3000');
 const wrtc = require('wrtc');
 ws.on('open' , () => {
 console.log("sended")
-ws.send(JSON.stringify({type : 'streamer_awake'}))
-      
+ws.send(JSON.stringify({type : 'streamer_awake'}))});
+let dc, pc;
+ws.on('message', async data => {
+    const message = JSON.parse(data.toString());
+    console.log(message.type)
+    if(message.type === 'clientoffers') {
+
+        pc = new wrtc.RTCPeerConnection([{ iceServers: 'stun:stun.l.google.com:19302' }]);
+        dc = pc.createDataChannel('server-to-browser');
+        
+        dc.onopen = () => {
+           let count = 0; const interval = setInterval(() => { if (dc.readyState === 'open') {
+             dc.send("Merhaba Browser! Mesaj #${++count} serverdan geliyorum."); }
+               else { clearInterval(interval); } })};
+        dc.onmessage = msg => console.log("dc.onmessage",msg.data);
+
+        pc.onicecandidate = ({ candidate }) => {
+          console.log("stream_candidate yollaniyor");
+          ws.send(JSON.stringify({ type:'streamer_candidate', candidate }));
+}
+        (async () => {
+            const offer = await pc.createOffer();
+            console.log("offer" , offer);
+            await pc.setLocalDescription(offer);
+            ws.send(JSON.stringify({ type: 'stream_offer',  offer }));
+        })();
+         
+
+          if (message.type === 'answer') {
+            await pc.setRemoteDescription(message.answer);
+          } 
+          else if (message.type === 'candidate') {
+            await pc.addIceCandidate(message.candidate);
+            console.log("ICE candidate eklendi.");
+    }
+
+    }
 });
-
-ws.on('message' , data => {
-  const message = JSON.parse(data.toString());
-
-  switch (message.type) {
-    case "client_offers" : 
-      console.log("offerstreameregeldi");
-      
-
-
-
-
-      break ; 
-  }
-
-})
 
 
